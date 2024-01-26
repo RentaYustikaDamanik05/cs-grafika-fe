9780c74cb9998ff3926d195733de0d0ffcdb2499
RentaYustikaDamanik05 <rentayustika@gmail.com>
2024-01-27 03:11:47 +0700

    fix: resolve test error

--- src/app/layout/layout.spec.tsx
+++ src/app/layout/layout.spec.tsx
@@ -1,10 +1,13 @@
 import { render } from '@testing-library/react';
 import { AppLayout } from './layout';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('AppLayout', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <AppLayout />
+      <BrowserRouter>
+        <AppLayout />
+      </BrowserRouter>
     );
     expect(baseElement).toBeTruthy();
   });
